9546e311ba56f664b40371d9158e11afc5882dbd
HiredMark <suner@syuleyman.com>
2021-11-16 10:05:58 +0000

    JavaScript Training

---
+++ JavaScript_Warmup/intro.js
@@ -0,0 +1,18 @@
+var globalGreeting = "Hello global variable!"; //Vars are always global and that can make them dangerous. 
+function readGlobalVar(){
+    if(true){
+    var globalGreeting = "Say Hi Instead" // Can be redeclared.
+    globalGreeting = "Say Yoyoooooooo" // locally modiifying a variable for the function but having the output just in the function.
+    console.log(globalGreeting)
+    }
+}
+
+console.log(globalGreeting)
+readGlobalVar()
+
+var varGreeting = "Here is Hello1 ! "
+console.log(varGreeting)
+var varGreeting = "Here is Hello2 ! "
+console.log(varGreeting)
+var varGreeting = "Here is Hello3 ! "
+console.log(varGreeting) // not idempotent. 
