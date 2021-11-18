ac59a6ee4e8f85d6130524b8bfc674fbc800ea29
HiredMark <suner@syuleyman.com>
2021-11-18 15:22:37 +0000

    learning JSX utils

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -5,11 +5,41 @@ import ReactDOM from "react-dom";
 
 // Create a React component
 
-const App = () => {
-    return <div>Hi There! </div>
+function getButtonText () {
+    return ""
 }
 
+const App = () => {
+    return (
 
+        <div>
+        Hi, Software Institute!
+    <ul>
+        <li> Hey </li>
+        <li> Hey there </li>
+        <li> Wassup? </li>
+        <li> How is it going? </li>
+        <li> Wazzzaaaaaaaaaaaaaaaaaaaaaaaaaaaa </li>
+        <li> Hooooooooow YOU doin'? </li>
+        </ul>
+    {/* </div>  */}
+    
+    {/* //this reads as HTML but is actually JSX. It is HTML for readability. BABEL process it out to JavaScript. 
+
+    //     <div>
+    //     <label class="important" for="name">Enter Name</label> - class becomes className - this avoids collision with class 
+    //     <input id="name" type="text" />
+    //     <button style="background-color: red; color: white;"> Submit </button> -style becomes JS for style options
+    //       {} allows for variables to be inserted. 
+    //     </div>
+    //  This HTML code snippet will not work. ^ 
+     */}
+        <label className="important" for="name"> Enter Name: </label> 
+        <input id="name" type="text" />
+        <button style = {{backgroundColor: "red", color: "white"}}> {buttonText} </button> 
+    </div>
+    )
+}
 
 
 // Take the React component and show it on the screen. 
@@ -17,6 +47,7 @@ const App = () => {
 ReactDOM.render(
 
     <App />,
+    document.querySelector("#root")
     
 
 );
